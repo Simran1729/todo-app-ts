cd35726e4895eec415c5c844b7bc5879812f3abd
Simran <bhatnagarsimran1306@gmail.com>
2024-03-13 02:26:59 +0530

    added login logic

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+
 function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        const response = fetch('http://localhost:3000/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        })
+
+        const data = await response.json();
+        if (data.token) {
+            localStorage.setItem("token", data.token)
+            window.location = "/todos";
+        } else {
+            alert("Invalid credentials");
+        }
+    };
+
     return (
-        <div>Login</div>
+        <div>
+            <div>
+                <h2>Login</h2>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                New here? <Link to="/signup">Signup</Link>
+                <button onClick={handleLogin}>Login</button>
+            </div>
+        </div>
     )
 }
 
