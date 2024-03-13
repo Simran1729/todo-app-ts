3f18428dad0699eea76a4b76817fd152966775c3
Simran <bhatnagarsimran1306@gmail.com>
2024-03-13 18:28:45 +0530

    Updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,56 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './Components/Login';
 import TodoList from './Components/TodoList';
 import Signup from './Components/Signup';
+import { authState } from '../store/authState';
+import { RecoilRoot, useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/todos' element={<TodoList />} />
-        <Route path='/' element={<Login />} />
-      </Routes>
-    </Router>
-  )
+    <RecoilRoot>
+      <Router>
+        <InitState />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/todos' element={<TodoList />} />
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
+  );
 }
 
-export default App
+function InitState() {
+  const setAuth = useSetRecoilState(authState);
+  const navigate = useNavigate();
+
+  const init = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch('http://localhost:3000/auth/me', {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      const data = await response.json();
+      if (data.username) {
+        setAuth({ token: data.token, username: data.username });
+        navigate("/todos");
+      } else {
+        navigate("/login");
+      }
+    } catch (e) {
+      navigate("/login");
+    }
+  }
+
+  useEffect(() => {
+    init();
+  }, [])
+
+  return <></>
+}
+
+
+export default App;
