c75dfdcb3356c54a5439d434d802fa77dfb62a7c
Simran <bhatnagarsimran1306@gmail.com>
2024-03-14 16:27:14 +0530

    bootstrapped the frontend using ts

---
+++ frontend-tsx/src/Components/Signup.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom";
+
+const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSingup = async () => {
+        const response = await fetch('http://localhost:3000/auth/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await response.json();
+        if (data.token) {
+            localStorage.setItem("token", data.token)
+            navigate("/todos");
+        } else {
+            alert("Error while signing up");
+        }
+    };
+
+
+    return (
+        <div style={{ justifyContent: 'center', display: 'flex', width: "100%" }}>
+            <div>
+                <h2>Singup</h2>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                Already signed up ? <Link to="/login">login</Link>
+                <button onClick={handleSingup}>Signup</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Signup
